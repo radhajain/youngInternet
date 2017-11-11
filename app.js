9d79b058c4e76eec74537f3d8dd448540ade7dbd
ggarza5 <gabe84700@gmail.com>
2017-11-10 22:23:25 -0800

    added artist schema

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var spawn = require('child-process').spawn;
+var spawn = require('child_process').spawn;
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var csv = require('fast-csv');
 
 var app = express();
 
@@ -57,10 +58,54 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+// mongoose.Promise = global.Promise;
+// var Cat = mongoose.model('Cat', { name: String });
+
+var artistSchema = newSchema({
+  _id: mongoose.Schema.Types.ObjectId,
+  intid: Number,
+  artist: String,
+  soundcloudFollows: [Number],
+  soundcloud_url: String,
+  instagram_url: String,
+  yt_channel_id: String,
+  t_channel_url: String,
+  twitter_name: String,
+  tyi_url: String,
+  artist_type: String,
+  type_label: String,
+  soundcloud_followers: Number,
+  location: String,
+  label: String,
+  signed: String,
+  monitored: String,
+  score: String,
+  growth_percent: String,
+  image: [String],
+  parent: String,
+  notes: String
+});
+
+
+// var kitty = new Cat({ name: 'Zildjian' });
+// kitty.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('meow');
+//   }
+// });
+
+
 var process = spawn('python3',["/home/bitnami/projects/sample/get_soundcloud_followers_today.py"]);
 setInterval(function() {
   spawn('python3',["/home/bitnami/projects/sample/get_soundcloud_followers_today.py"])
-}, 120)
+  function() {
+
+  }
+}, 10 * 1000)
 module.exports = app;
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
